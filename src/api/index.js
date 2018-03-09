593509f294344d31f592e1facd32bd74eb732624
ezra <linux.esdras@gmail.com>
2018-03-08 23:38:10 -0300

    Add api

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
 
+const getHeaders = () => {
+  const token = localStorage.userJWT;
+  return {
+    headers: { Authorization: `Bearer ${token}` }
+  };
+};
+
 export default {
   user: {
     signin: data => axios.post('/api/v1/auth', data).then(res => res.data),
     signup: user => axios.post('/api/v1/users', user).then(res => res.data)
+  },
+  post: {
+    fetchAll: () => axios.get('/api/v1/posts', getHeaders()).then(res => res.data),
+    create: data => axios.post('/api/v1/posts', data, getHeaders()).then(res => res.data)
   }
 }
