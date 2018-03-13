4badb183c0f4f851cf1fd65564cbf532138c0bba
ezra <linux.esdras@gmail.com>
2018-03-13 14:27:44 -0300

    Add fetch profile

--- src/api/index.js
+++ src/api/index.js
@@ -11,7 +11,8 @@ export default {
   user: {
     signin: data => axios.post('/api/v1/auth', data).then(res => res.data),
     signup: user => axios.post('/api/v1/users', user).then(res => res.data),
-    fetchCurrent: () => axios.get('/api/v1/users', getHeaders()).then(res => res.data)
+    fetchCurrent: () => axios.get('/api/v1/users', getHeaders()).then(res => res.data),
+    fetchUser: user => axios.get(`/api/v1/users/${user}`).then(res => res.data)
   },
   post: {
     fetchAll: () => axios.get('/api/v1/posts', getHeaders()).then(res => res.data),
--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -1,47 +1,55 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Grid, Dimmer, Image, Loader } from 'semantic-ui-react';
 
+import api from '../../api';
+
+// TODO: change default avatar
+const defaultAvatar = '/assets/avatar.jpg';
+
 class ProfilePage extends React.Component {
 
+  state = {};
+
   componentDidMount() {
-    /*
-     * TODO: Fetch user
-     */
+    // TODO: use redux?
+    api
+      .user
+      .fetchUser(this.props.match.params.username)
+      .then(user => this.setState({ user }));
   }
 
   render() {
-    const { user } = this.props;
+    const { user } = this.state;
+
+    console.log('user', user);
 
     return (
       <Grid container>
         <Grid.Row centered className='profile-header'>
-          <Dimmer active={user}>
+          <Dimmer active={!user}>
             <Loader size='medium'>Loading</Loader>
           </Dimmer>
-          <Grid.Column width={3} textAlign='center'>
-            <Image
-              avatar
-              circular
-              size='medium'
-              className='profile-image'
-              src={user.avatar}
-            />
-            <Link to={`/profile/@${user.username}`}>
-              <span className='profile-username'>
-                @{user.name}
-              </span>
-            </Link>
-          </Grid.Column>
+          {user &&
+              <Grid.Column width={3} textAlign='center'>
+                <Image
+                  avatar
+                  circular
+                  size='medium'
+                  className='profile-image'
+                  src={defaultAvatar}
+                />
+                <Link to={`/profile/@${user.username}`}>
+                  <span className='profile-username'>
+                    @{user.name}
+                  </span>
+                </Link>
+              </Grid.Column>
+          }
         </Grid.Row>
       </Grid>
     );
   }
 }
-
-ProfilePage.propTypes = {
-  user: PropTypes.object
-};
 
 export default ProfilePage;
